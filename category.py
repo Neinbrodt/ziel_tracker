3af6a064fa9055de91029233ca50c0a4950e5bee
Neinbrodt <114073400+nbrodt@users.noreply.github.com>
2023-01-25 18:39:29 +0100

    chaaanges

--- category.py
+++ category.py
@@ -1,9 +1,9 @@
-from tracker import tracker
 import csv
-import sys
 from datetime import datetime
+
 import pandas as pd
-import visualization
+
+from tracker import tracker
 
 
 class category:
@@ -30,22 +30,32 @@ class category:
             category.existence()
         else:
             writer = csv.writer(open('categories.csv', 'a+', newline=''), delimiter=',')
-            writer.writecolumn([cat_name])
+            writer.writerow([cat_name])
             print(f"Alles klar! Du kannst jetzt {cat_name} tracken.")
             category.t_track()
 
     @staticmethod
     def t_track():
+        today = datetime.today().strftime('%Y-%m-%d')
         go = input("Möchtest du für heute etwas tracken? [y/n]")
         if go == "y":
             print("Hier sind deine Kategorien. Wähle, indem Du eine Zahl eingibst.")
-            columns = 0
-            for column in open("categories.csv"):
-                columns += 1
-                print(f"[{columns}] - {column}")
+            rows = 0
+            for row in open("categories.csv"):
+                rows += 1
+                print(f"[{rows}] - {row}")
             # TODO: Nummer hinzufügen / Datum hinzufügen zum CSV
             cat_number = int(input())
-            # cat.add_today()
+            with open("categories.csv", "r") as cat_list:
+                reader = csv.reader(cat_list)
+                for row in reader:
+                    if cat_number != row:
+                        print("It works!")
+                    else:
+                        writer = csv.writer(open('categories.csv', 'a+', newline=''), delimiter=',')
+                        row.append(today)
+                        writer.writerow(row)
+
 
     def analyze(self):
         go = input("Möchtest du deine Gewohnheiten analysieren? [y/n]")
@@ -60,18 +70,10 @@ class category:
         print("byeee <3")
 
     # getters and setters for the category's name
-    def get_name(self):
-        return self.name
-
-    def set_name(self, new_name):
-        self.name = new_name
 
     # methods to handle data
     """add only the current date to the tracker"""
 
-    def add_today(self):
-        today = datetime.today().strftime('%Y-%m-%d')
-        self.t.add_new(self, today)
 
     """change any date from checked to unchecked or vice-versa"""
 
